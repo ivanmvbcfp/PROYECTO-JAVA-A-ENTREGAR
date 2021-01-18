92d610814aa7109cec6b98d375aacbf930616f37
Ivan <ivannmoreno2001@gmail.com>
2021-01-18 13:52:08 +0100

    SNAPSHOT 3

---
+++ Main3.java
@@ -0,0 +1,13 @@
+
+package misrc;
+
+
+public class Main3 {
+
+
+    public static void main(String[] args) {
+        for (int i=0; i<= 100; i++) {
+                System.out.println(i);
+    }
+  }
+}
